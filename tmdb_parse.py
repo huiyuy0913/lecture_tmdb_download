eeef349c82f90300a048c09cb4f2c79a94fc9ac8
Huiyu Yang <heidi@214.211.21.198.tigernet.wifi.dyn.clemson.edu>
2022-10-06 12:12:20 -0400

    parse multiple files

--- tmdb_parse.py
+++ tmdb_parse.py
@@ -9,27 +9,34 @@ if not os.path.exists("parsed_files"):
 
 df = pandas.DataFrame()
 
-json_file_name = "json_files/tmdb_1032400.json"
+for json_file_name in glob.glob("json_files/*.json"):
 
-f = open(json_file_name,"r")
-json_data = json.load(f)
-f.close()
+	# json_file_name = "json_files/tmdb_1032400.json"
+	f = open(json_file_name,"r")
+	json_data = json.load(f)
+	f.close()
 
 
-movie_id = json_data['id']
-movie_title = json_data['title']
+	movie_id = json_data['id']
+	movie_title = json_data['title']
+	vote_average = json_data['vote_average']
+	vote_count = json_data['vote_count']
 
-print(movie_id)
-print(movie_title)
+	# print(movie_id)
+	# print(movie_title)
 
-df = pandas.concat([df,
+	df = pandas.concat([df,
 
-  pandas.DataFrame.from_records([{
-	'movie_id': movie_id,
-	'movie_title': movie_title
-	}])
-  ])
+	  pandas.DataFrame.from_records([{
+		'movie_id': movie_id,
+		'movie_title': movie_title,
+		'vote_average': vote_average,
+		'vote_count': vote_count
+		}])
+	  ])
 
 # print(df)
 
 df.to_csv("parsed_files/tmdb_dataset.csv",index=False)
+
+print("done")
